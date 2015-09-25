4cafd7efff9e4e779ede90c45c56b2e7eda8dbc3
Pascal de Bruijn <pmjdebruijn@pcode.nl>
2015-09-25 18:33:49 +0200

    catsfc: update to git master

--- package/catsfc/catsfc.mk
+++ package/catsfc/catsfc.mk
@@ -3,13 +3,13 @@
 # CATSFC
 #
 ################################################################################
-CATSFC_VERSION = 361b6927ef6af4e0913f18909cb841fd192e69d1
+CATSFC_VERSION = 64ffe869caba187b8c7205ad6182f5c603008128
 CATSFC_SITE = $(call github,libretro,CATSFC-libretro,$(CATSFC_VERSION))
 CATSFC_TARGET = libcatsfc.so
 
 define CATSFC_BUILD_CMDS
-	$(MAKE) CC="$(TARGET_CC)" CXX="$(TARGET_CXX)" \
-		CFLAGS="-I. -Isource $(TARGET_CFLAGS)" LD="$(TARGET_LD)" \
+	$(MAKE) USE_BLARGG_APU=0 \
+		CC="$(TARGET_CC)" CXX="$(TARGET_CXX)" LD="$(TARGET_LD)" \
 		TARGET="$(CATSFC_TARGET)" -C $(@D) all
 endef
 
